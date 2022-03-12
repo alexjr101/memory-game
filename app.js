a2b96b86d83567035f247349cf231f6a588e4b3a
alexjr101 <alex.j.ribes@gmail.com>
2022-03-11 19:10:08 -0600

    Basic Setup
    -Basic setup of HTML, CSS, and JS files
    - Imported images and created an array (the 'grid') of 6 pairs of card objects shuffled in a random order

---
+++ app.js
@@ -0,0 +1,51 @@
+const cardArray = [
+    {
+        name: 'sun',
+        img: 'images/sun.png'
+    },
+    {
+       name: 'cloud' ,
+       img: 'images/cloud.png'
+    },
+    {
+        name: 'lightning' ,
+        img: 'images/lightning.png'
+     },
+     {
+        name: 'moon' ,
+        img: 'images/moon.jpeg'
+     },
+     {
+        name: 'rainbow' ,
+        img: 'images/rainbow.jpeg'
+     },
+     {
+        name: 'star' ,
+        img: 'images/star.png'
+     },
+     {
+        name: 'sun',
+        img: 'images/sun.png'
+    },
+    {
+       name: 'cloud' ,
+       img: 'images/cloud.png'
+    },
+    {
+        name: 'lightning' ,
+        img: 'images/lightning.png'
+     },
+     {
+        name: 'moon' ,
+        img: 'images/moon.jpeg'
+     },
+     {
+        name: 'rainbow' ,
+        img: 'images/rainbow.jpeg'
+     },
+     {
+        name: 'star' ,
+        img: 'images/star.png'
+     }
+];
+cardArray.sort(() => 0.5 - Math.random());
